080f8c53bdb3ef01726e93ba57071400584d44e0
Vladyslav Khakov <vladhakov20@gmail.com>
2024-07-04 11:15:47 -0700

    minor prettier fixes

--- src/degen-fund-sdk/utils.ts
+++ src/degen-fund-sdk/utils.ts
@@ -1,4 +1,5 @@
-import { BN, web3 } from "@coral-xyz/anchor";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { BN, web3 } from '@coral-xyz/anchor';
 
 export async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -12,28 +13,20 @@ export function getPubkeyFromStr(key: string) {
   }
 }
 
-export async function getMultipleAccountsInfo(
-  connection: web3.Connection,
-  pubkeys: web3.PublicKey[],
-  opt?: { retry?: boolean; duration?: number }
-) {
+export async function getMultipleAccountsInfo(connection: web3.Connection, pubkeys: web3.PublicKey[], opt?: { retry?: boolean; duration?: number }) {
   opt = opt ?? {};
   opt.retry = opt.retry ?? true;
   opt.duration = opt.duration ?? 2000;
   const { duration, retry } = opt;
-  const res = await connection
-    .getMultipleAccountsInfo(pubkeys)
-    .catch(async () => {
-      if (retry) {
-        await sleep(duration);
-        return await connection
-          .getMultipleAccountsInfo(pubkeys)
-          .catch((getMultipleAccountsInfoError) => {
-            return null;
-          });
-      }
-      return null;
-    });
+  const res = await connection.getMultipleAccountsInfo(pubkeys).catch(async () => {
+    if (retry) {
+      await sleep(duration);
+      return await connection.getMultipleAccountsInfo(pubkeys).catch((getMultipleAccountsInfoError) => {
+        return null;
+      });
+    }
+    return null;
+  });
   return res;
 }
 
@@ -55,12 +48,12 @@ export function calcDecimalValue(value: number, decimals: number): number {
 export const parseUnits = (amount: string | number, decimals: number): BN => {
   const base = new BN(10).pow(new BN(decimals));
   // Split the input to handle decimal amounts correctly
-  const parts = amount.toString().split(".");
+  const parts = amount.toString().split('.');
   let bnAmount = new BN(parts[0]).mul(base);
 
   if (parts.length > 1) {
-    let fraction = parts[1];
-    let fractionBase = new BN(10).pow(new BN(fraction.length));
+    const fraction = parts[1];
+    const fractionBase = new BN(10).pow(new BN(fraction.length));
     let fractionAmount = new BN(fraction);
     fractionAmount = fractionAmount.mul(base).div(fractionBase);
     bnAmount = bnAmount.add(fractionAmount);
@@ -75,6 +68,6 @@ export const formatUnits = (amount: BN, decimals: number): string => {
   const fraction = amount.mod(base).toString(10);
 
   // Properly format the fraction part to remove leading zeroes
-  const paddedFraction = fraction.padStart(decimals, "0").replace(/0+$/, "");
-  return `${integer.toString(10)}${paddedFraction ? "." + paddedFraction : ""}`;
+  const paddedFraction = fraction.padStart(decimals, '0').replace(/0+$/, '');
+  return `${integer.toString(10)}${paddedFraction ? '.' + paddedFraction : ''}`;
 };
